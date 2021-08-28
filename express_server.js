93a73238dec495948e606f0a2dba1dd845fffd3f
Eti Daniel <danieleti@gmail.com>
2021-08-27 22:15:57 -0400

    adding post request to the server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 //This tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs");
@@ -30,10 +32,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req,res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.send(generateRandomString());
+});
+
+function generateRandomString() {
+  // Generate a random number, convert it to a string using
+  // a radix of 36 (which will include all alph-numericals)
+  // then cut a 6 char substring.
+  return Math.random().toString(36).substr(2, 6)
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
