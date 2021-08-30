05970cf667105b3ecfe53a6bef25cc3949255adb
Eti Daniel <danieleti@gmail.com>
2021-08-29 21:34:13 -0400

    Adding a method to redirect to the longURL when pressing the link with the shortURL in the urls_show page

--- express_server.js
+++ express_server.js
@@ -41,10 +41,22 @@ app.get("/urls/:shortURL", (req,res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req,res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.send(generateRandomString());
+
+  // Generate a random 6 char string
+  const shortURL = generateRandomString();
+
+  // Save generated shortURL into our DB
+  urlDatabase[shortURL] = req.body.longURL;
+
+  // Redirect the browser to the shortURL
+  res.redirect(`/urls/${shortURL}`);
 });
 
 function generateRandomString() {
